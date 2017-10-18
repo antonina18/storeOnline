83ef474c49de9e7831b3cf0ceaf3705daf733e41
wisnienka <katarzyna.sowa@silvair.com>
2017-10-18 12:46:01 +0200

    ci + cd

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,13 @@
+package store;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan( basePackages = {"store"} )
+@ComponentScan("store")
 public class Application {
 
     public static void main(String[] args) {
